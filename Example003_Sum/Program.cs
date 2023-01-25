8b9d5935933421e5e89f53965bae315de198438f
Daiza <salkodg@gmail.com>
2023-01-25 23:16:10 +0500

    add Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int NumberA = 3;
-int NumberB = 5;
+﻿int NumberA = new Random() .Next(1, 101);
+Console.WriteLine(NumberA);
+int NumberB = new Random() .Next(1, 101);
+Console.WriteLine( NumberB);
 Console.WriteLine(NumberA + NumberB);
 
 //int NumberA = 3;
