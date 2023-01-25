6cf6b5c4e5de99d353d2bf73a3f1399a659e3319
Daiza <salkodg@gmail.com>
2023-01-25 22:44:55 +0500

    Create project Example004_DivisionResult

---
+++ Example004_DivisionResult/Program.cs
@@ -0,0 +1,8 @@
+﻿double NumberA = 354325;
+double NumberB = 2;
+Console.WriteLine(NumberA / NumberB);
+
+//double NumberA = 3;
+//double NumberB = 5;
+//double result = NumberA / NumberB;
+//Console.WriteLine(result);
