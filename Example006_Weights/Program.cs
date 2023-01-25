cec4f072de4b7e75c796220ad8842eefea76e86a
Daiza <salkodg@gmail.com>
2023-01-26 00:30:37 +0500

    Create project Example006_Weights

---
+++ Example006_Weights/Program.cs
@@ -0,0 +1,30 @@
+﻿int a = new Random().Next(0, 101);
+int b = new Random().Next(0, 101);
+int c = new Random().Next(0, 101);
+int d = new Random().Next(0, 101);
+int e = new Random().Next(0, 101);
+int f = new Random().Next(0, 101);
+Console.Write("a = ");
+Console.WriteLine(a);
+Console.Write("b = ");
+Console.WriteLine(b);
+Console.Write("c = ");
+Console.WriteLine(c);
+Console.Write("d = ");
+Console.WriteLine(d);
+Console.Write("e = ");
+Console.WriteLine(e);
+Console.Write("f = ");
+Console.WriteLine(f);
+
+int max = a;
+ 
+if(a > max ) max = a;
+if(b > max ) max = b;
+if(c > max ) max = c;
+if(d > max ) max = d;
+if(e > max ) max = e;
+if(f > max ) max = f;
+
+Console.Write("max = ");
+Console.Write(max);
