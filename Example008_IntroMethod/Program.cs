129e9e8b7c96329b2b871ddc0f8d2beb011130ca
Daiza <salkodg@gmail.com>
2023-01-28 15:34:15 +0500

    Edit project

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -21,7 +21,7 @@ int c3 = 33;
 // int max3 = Max(a3, b3, c3);
 // int max = Max(max1, max2, max3);
 
-int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+int max = Max(Max(a1, b1, c1),Max(a2, b2, c2), Max(a3, b3, c3));
 
 // if (b1 > max) max = b1; / / "Стихийный" метод
 // if (c1 > max) max = c1;
