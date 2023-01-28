0d5b1ffa7415272706bfd99b1fb34fc87005fcda
Daiza <salkodg@gmail.com>
2023-01-28 20:18:33 +0500

    completed the second task

---
+++ Seminar001/Homework/Example002_seminar1/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.Clear();
+// ________Вариант1________
+Console.WriteLine("Ведите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите число b");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+// От себя добавил пару вариантов исполнения
+//____________Вариант2___________
+/* Console.WriteLine("Ведите число a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ведите число b");
+int b = int.Parse(Console.ReadLine());
+if (a == b)
+{
+    Console.WriteLine("Эти числа равны!");
+}
+else
+{
+    if (a > b)
+    {
+        if (a >= b * 2)
+        {
+            Console.WriteLine($"Число {a} больше, как минимум в два раза!");
+        }
+        else
+        {
+            Console.WriteLine($"Число {a} больше");
+        }
+    }
+    else
+    {
+        if (b >= a * 2)
+        {
+            Console.WriteLine($"Число {b} больше, как минимум в два раза!");
+        }
+        else
+        {
+            Console.WriteLine($"Число {b} больше");
+        }
+    }
+} */
+// ____________Вариант3(Метод)___________
+/* void Comparrison(int a, int b)
+{
+    if (a == b)
+    {
+        Console.WriteLine("Эти числа равны!");
+    }
+    else
+    {
+        if (a > b)
+        {
+            if (a >= b * 2)
+            {
+                Console.WriteLine($"Число {a} больше, как минимум в два раза!");
+            }
+            else
+            {
+                Console.WriteLine($"Число {a} больше");
+            }
+        }
+        else
+        {
+            if (b >= a * 2)
+            {
+                Console.WriteLine($"Число {b} больше, как минимум в два раза!");
+            }
+            else
+            {
+                Console.WriteLine($"Число {b} больше");
+            }
+        }
+    }
+}
+
+Console.WriteLine("Ведите сначала число a, потом число b");
+Comparrison(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())); */
