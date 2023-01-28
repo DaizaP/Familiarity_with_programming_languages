10df00e8aa0dfee29b6ad8d82dca89f06cc81219
Daiza <salkodg@gmail.com>
2023-01-28 21:46:19 +0500

    (last)completed the fourth tas

--- Seminar001/Homework/Example004_seminar1/Program.cs
+++ Seminar001/Homework/Example004_seminar1/Program.cs
@@ -18,7 +18,7 @@ int max = a;
  if(a > max ) max = a;
 if(b > max ) max = b;
 if(c > max ) max = c;
-Console.Write($"max = {max}");
+Console.Write($"{a}, {b}, {c} -> {max}");
 
 //_________Вариант2___________
 
