acfd41a63b783686379c53357979540313c10a91
Daiza <salkodg@gmail.com>
2023-02-23 18:12:27 +0500

    Finally completed

---
+++ Seminar007/ExtraExample001/Program.cs
@@ -0,0 +1,88 @@
+﻿// *Доп1
+// На днях Иван у себя в прихожей выложил кафель,
+// состоящий из квадратных черных и белых плиток.
+// Прихожая Ивана имеет квадратную форму 4х4, вмещающую 16 плиток.
+// Теперь Иван переживает, что узор из плиток, который у него получился,
+// может быть не симпатичным. С точки зрения дизайна симпатичным узором считается тот,
+// который не содержит в себе квадрата 2х2, состоящего из плиток одного цвета.
+// Входные данные:
+// BWBW
+// BBWB
+// WWBB
+// BWWW
+// Ответ:
+// Yes
+
+// Входные данные:
+// BBWB
+// BBWB
+// WWBW
+// BBWB
+// Ответ:
+// No
+using System;
+using static System.Console;
+
+Clear();
+
+
+string Read(string s)
+{
+    (int posLeft, int posTop) = Console.GetCursorPosition();
+    string textFromConsole = Console.ReadLine();
+    posLeft = posLeft + textFromConsole.Length;
+    Console.SetCursorPosition(posLeft, posTop);
+    Console.Write($"{s}");
+    return textFromConsole;
+}
+
+char[,] GetRandomArrayTiles()
+{
+    string symbol = "BW";
+    Random rnd = new Random();
+    char[,] result = new char[4, 4];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = symbol[rnd.Next(0, symbol.Length)];
+        }
+    }
+    return result;
+}
+
+void PrintArray(char[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+void ValidTiles(char[,] inArray)
+{
+    bool valid = true;
+    for (int i = 1; i < inArray.GetLength(0) - 1; i++)
+    {
+        for (int j = 1; j < inArray.GetLength(1) - 1; j++)
+        {
+            if ((inArray[i, j] == inArray[i - 1, j] && inArray[i, j] == inArray[i, j - 1] && inArray[i, j] == inArray[i - 1, j - 1]) ||
+                (inArray[i, j] == inArray[i - 1, j] && inArray[i, j] == inArray[i, j + 1] && inArray[i, j] == inArray[i - 1, j + 1]) ||
+                (inArray[i, j] == inArray[i + 1, j] && inArray[i, j] == inArray[i, j - 1] && inArray[i, j] == inArray[i + 1, j - 1]) ||
+                (inArray[i, j] == inArray[i + 1, j] && inArray[i, j] == inArray[i, j + 1] && inArray[i, j] == inArray[i + 1, j + 1]))
+            {
+                // Write($"[{i + 1}, {j + 1}]"); // Для ручной проверки правильности выполнения
+                valid = false;
+                break;
+            }
+        }
+    }
+    Write("Ответ: ");
+    Write(valid);
+}
+char[,] array = GetRandomArrayTiles();
+PrintArray(array);
+ValidTiles(array);
---
+++ Seminar007/ExtraExample002/Program.cs
@@ -0,0 +1,166 @@
+﻿/* **Доп
+Игра «Жизнь» была придумана английским математиком Джоном Конвейем в 1970 году.
+Впервые описание этой игры опубликовано в октябрьском выпуске (1970) журнала Scientic American,
+в рубрике «Математические игры» Мартина Гарднера.
+
+Место действия этой игры – «вселенная» – это размеченная на клетки поверхность.
+Каждая клетка на этой поверхности может находиться в двух состояниях: быть живой или быть мертвой.
+Клетка имеет восемь соседей. Распределение живых клеток в начале игры называется первым поколением.
+Каждое следующее поколение рассчитывается на основе предыдущего по таким правилам:
+
+1)пустая(мертвая) клетка с ровно тремя живыми клетками-соседями оживает;
+2)если у живой клетки есть две или три живые соседки, то эта клетка продолжает жить;
+в противном случае (если соседок меньше двух или больше трех)
+клетка умирает(от «одиночества» или от «перенаселенности»).
+В этой задаче рассматривается игра «Жизнь» на торе.
+Представим себе прямоугольник размером n строк на m столбцов.
+Для того, чтобы превратить его в тор мысленно «склеим» его верхнюю сторону с нижней, а левую с правой.
+
+Таким образом, у каждой клетки, даже если она раньше находилась на границе прямоугольника,
+теперь есть ровно восемь соседей.
+
+Ваша задача состоит в том, чтобы найти конфигурацию клеток, которая будет через k поколений от заданного.
++ - живая клетка
+. - мёртвая
+
+n m k(4 ≤ n, m ≤ 100; 0 ≤ k ≤ 100).
+Входные данные:
+5 строки, 5 столбцы, 1 поколения
+++...
+..++.
+.+...
+..+..
+...+.
+Ответ:
+.+.++
++.+..
+.+.+.
+..+..
+.++..
+
+Входные данные:
+5 5 5
+++...
+..++.
+.+...
+..+..
+...+.
+Ответ:
+.+++.
+.+...
+.+...
+..+.
+.....
+Входные данные:
+4 7 5
+.+.+.+.
++.+.+.+
+.+.+.+.
++.+.+.+
+Ответ:
+.......
+.......
+.......
+....... */
+
+using System;
+using static System.Console;
+
+Clear();
+
+string Read(string s)
+{
+    (int posLeft, int posTop) = Console.GetCursorPosition();
+    string textFromConsole = Console.ReadLine();
+    posLeft = posLeft + textFromConsole.Length;
+    Console.SetCursorPosition(posLeft, posTop);
+    Console.Write($"{s}");
+    return textFromConsole;
+}
+
+int[,] GetRandomArray(int lines, int columns)
+{
+    Random rnd = new Random();
+    int[,] result = new int[lines, columns];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = rnd.Next(0, 2);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[i, j] == 1) { Write("o "); }
+            else { Write("_ "); }
+        }
+        WriteLine();
+    }
+}
+// [условие] ? [действие при true]:[действие при false]
+int ThorX(int[,] inArray, int lines)
+{
+    lines = (lines < 0 ? lines += inArray.GetLength(0) : lines);
+    lines = (lines < inArray.GetLength(0) ? lines : lines - inArray.GetLength(0));
+    return lines;
+}
+int ThorY(int[,] inArray, int columns)
+{
+    columns = (columns < 0 ? columns += inArray.GetLength(1) : columns);
+    columns = (columns < inArray.GetLength(1) ? columns : columns - inArray.GetLength(1));
+    return columns;
+}
+
+int NeighborCount(int lines, int columns, int[,] inArray)
+{
+    int neigbors = 0;
+
+    if (inArray[ThorX(inArray, lines - 1), ThorY(inArray, columns)] == 1) { neigbors++; }
+    if (inArray[ThorX(inArray, lines + 1), ThorY(inArray, columns)] == 1) { neigbors++; }
+    if (inArray[ThorX(inArray, lines), ThorY(inArray, columns - 1)] == 1) { neigbors++; }
+    if (inArray[ThorX(inArray, lines), ThorY(inArray, columns + 1)] == 1) { neigbors++; }
+    if (inArray[ThorX(inArray, lines - 1), ThorY(inArray, columns - 1)] == 1) { neigbors++; }
+    if (inArray[ThorX(inArray, lines - 1), ThorY(inArray, columns + 1)] == 1) { neigbors++; }
+    if (inArray[ThorX(inArray, lines + 1), ThorY(inArray, columns - 1)] == 1) { neigbors++; }
+    if (inArray[ThorX(inArray, lines + 1), ThorY(inArray, columns + 1)] == 1) { neigbors++; }
+
+
+    return neigbors;
+}
+void LifeOrDeath(int[,] inArray, int age)
+{
+    int cycle = 1;
+    int[,] result = new int[inArray.GetLength(0), inArray.GetLength(1)];
+    while (cycle != age + 1)
+    {
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < inArray.GetLength(1); j++)
+            {
+                int alive = NeighborCount(i, j, inArray);
+                if (inArray[i, j] == 1 && (alive == 2 || alive == 3)) { result[i, j] = 1; }
+                if (inArray[i, j] == 1 && (alive < 2 || alive > 3)) { result[i, j] = 0; }
+                if (inArray[i, j] == 0 && alive == 3) { result[i, j] = 1; }
+            }
+        }
+        WriteLine($"Поколение {cycle}.");
+        PrintArray(result); //Ручная проверка и вывод каждого поколения
+        inArray = result;
+        cycle++;
+    }
+}
+
+Write($"Введите кол-во строк и столбцов для массива и кол-во поколений через Enter: ");
+int lines = int.Parse(Read(" "));
+int columns = int.Parse(Read(" "));
+int age = int.Parse(ReadLine());
+int[,] array = GetRandomArray(lines, columns);
+PrintArray(array);
+LifeOrDeath(array, age);
